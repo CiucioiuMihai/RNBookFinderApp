3d409de149a027a796f888d11002a6a91678d168
CiucioiuMihai <40771@DESKTOP-CH8209N>
2025-06-02 23:33:20 +0300

    Merge si pe telefon acuma da ii incompleta

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,9 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push('cjs');
+const config = getDefaultConfig(__dirname);
+config.resolver.assetExts.push('cjs');
 
-module.exports = defaultConfig;
+config.resolver.unstable_enablePackageExports = false;
+
+module.exports = config;
